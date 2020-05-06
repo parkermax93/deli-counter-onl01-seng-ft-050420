6d2eff587acdebc9248e9c3eba1bb10f185e54af
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 18:30:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = [] 
+
+def line(place)
+  if array.count == 0 
+    puts "The line is currently empty."
+  else
+    the_line = "The line is currently:"
+    array.each_with_index do |name, index|
+      the_line += "#{index+1}. #{Name}"
+  end
+  puts the_line
+end
+end
