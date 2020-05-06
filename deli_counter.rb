40fa53fe30f93d049794067934a8d34621b54119
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 18:36:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,15 @@ def line (array)
    puts response
    end
 end
+
+def take_a_number (array.name)
+  array.push(name)
+  puts "Welcome #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.count == 0 
+    puts "There is nobody waiting to be served!"
+  elseputs "Currently serving #{array.shift}."
+end
+end
