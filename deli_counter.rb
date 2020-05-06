7dfaa672ec555adc713b6aaa74278af576a32721
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 18:38:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number (array,name)
   array.push(name)
-  puts "Welcome #{name}. You are number #{array.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
